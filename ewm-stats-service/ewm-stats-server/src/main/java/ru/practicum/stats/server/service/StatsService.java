20a2a6a630ea549c6b6233688d6db5508f038d87
EkaterinaChuyko <kylefoxmay@yandex.ru>
2025-12-20 10:51:57 +0300

    исправлен чекстайл по сборке maven

--- ewm-stats-service/ewm-stats-server/src/main/java/ru/practicum/stats/server/service/StatsService.java
+++ ewm-stats-service/ewm-stats-server/src/main/java/ru/practicum/stats/server/service/StatsService.java
@@ -8,5 +8,6 @@ import java.util.List;
 
 public interface StatsService {
     void saveHit(HitDto dto);
+
     List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique);
 }
