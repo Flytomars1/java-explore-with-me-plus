f54dfd137220ac4f9ac407e2e0c80136b4d3c50e
flytomars <tramp.box@gmail.com>
2025-12-23 12:53:52 +0300

    добавлен скелет основного сервиса, пока что три основных модели и репозитории, чтобы можно было собрать проект

---
+++ ewm-main-service/src/main/java/ru/practicum/main/model/Event.java
@@ -0,0 +1,27 @@
+package ru.practicum.main.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "events")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "initiator_id")
+    private User initiator;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    private String title;
+    private String annotation;
+}
